168b45a53d8ea23eaa61fc5dfdcb2f5100436293
Jeff Escalante <hello@jenius.me>
2016-05-05 17:02:42 -0400

    progress

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const Rooftop = require('..')
 const test = require('ava')
 
@@ -18,7 +20,36 @@ test('initializes with correct options', (t) => {
   t.truthy(api)
 })
 
-test('proxy', (t) => {
+test('errors when trying to get posts with wrong site', (t) => {
   const api = Rooftop.new({ name: 'foo', apiToken: 'bar' })
-  api.foo
+  return api.posts.get().catch((res) => {
+    t.is(res.error.toString(), 'Error: getaddrinfo ENOTFOUND foo.rooftopcms.io foo.rooftopcms.io:80')
+  })
+})
+
+test('errors when trying to get posts with wrong api key', (t) => {
+  const api = Rooftop.new({ name: process.env.name, apiToken: 'bar' })
+  return api.posts.get().catch((res) => {
+    t.is(res.status.code, 403)
+  })
+})
+
+test('errors when trying to get nonexistant post type', (t) => {
+  const api = Rooftop.new({
+    name: process.env.name,
+    apiToken: process.env.token
+  })
+
+  return api.fooBars.get().catch((r) => t.is(r.status.code, 404))
+})
+
+test('gets data when valid data is provided', (t) => {
+  const api = Rooftop.new({
+    name: process.env.name,
+    apiToken: process.env.token
+  })
+
+  return api.posts.get().then((res) => {
+    t.truthy(res.length > 0)
+  })
 })
